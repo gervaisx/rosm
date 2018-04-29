94c79c8a014c7ff90f5922758f33f8d170b34cf7
gervaisx <gervaisx@gmail.com>
2018-04-29 14:09:00 -0400

    Update

--- models/type_milieu.py
+++ models/type_milieu.py
@@ -16,3 +16,7 @@ class TypeMilieuModel(db.Model):
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
